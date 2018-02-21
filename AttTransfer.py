02a452a93dce6706cf20895f78ec3835da47a004
mTorresCOD <35778695+mTorresCOD@users.noreply.github.com>
2018-02-21 13:00:31 -0500

    Reverting to clean version
    Pre SDE Version

--- AttTransfer.py
+++ AttTransfer.py
@@ -1,74 +1,49 @@
 import arcpy
 
-# arcpy.env.workspace = "D:/Test.gdb"
-arcpy.env.workspace = r"C:\Users\MiguelTo\AppData\Roaming\ESRI\Desktop10.3\ArcCatalog\PW_Tax_sql_PW.sde"
+arcpy.env.workspace = "D:/Test.gdb"
 arcpy.env.overwriteOutput = True
-#
-# roadfeatures = "D:/Test.gdb/roadsSubset"
-roadfeatures = r"C:\Users\MiguelTo\AppData\Roaming\ESRI\Desktop10.3\ArcCatalog\A1_durham-gis.sde\GIS_Data.A1.Road_Features\GIS_Data.A1.Roads"
-# patchfeatures = "D:/Test.gdb/patch"
-patchfeatures = r"C:\Users\MiguelTo\AppData\Roaming\ESRI\Desktop10.3\ArcCatalog\PW_Tax_sql_PW.sde\PW.PW.PatchingPolygon"
 
-#joinFeatures = r"D:\Test.gdb\roadsSubset"
-#targetFeatures = r"D:\Test.gdb\patch"
-outputFeatures = r"D:\Test.gdb\PatchSJPoly3"
-# outputFeatures = r"in_memory\tempFeatures"
-# patch_SpatialJoin8 = "C:\\Users\\MiguelTo\\Documents\\ArcGIS\\Default.gdb\\patch_SpatialJoin10"
-
-# select Patches that need to be updated.
+roadfeatures = "D:/Test.gdb/roadsSubset"
+patchfeatures = "D:/Test.gdb/patch"
+outputFeatures = r"in_memory\tempFeatures"
 
 
 def selectdata():
+    # select Patches that need to be updated.
     print "Beginning Data Selection."
     # make patch feature and roads layers
     print "Creating Patches Layer."
     arcpy.MakeFeatureLayer_management(patchfeatures, "patchlayer")
 
     # Select patches with no ROADID
-    print "Select 'NULL' Patches."
+    print "Selecting 'NULL' Patches."
     arcpy.SelectLayerByAttribute_management("patchlayer", "NEW_SELECTION", ' "ROADID" IS NULL ')
-    print "Data Acquisition Completed \n"
+    print "Data Selection Complete. \n"
 
-def testdata():
-    fields = ['FACILITYID', 'ROADID']
-    with arcpy.da.SearchCursor("patchlayer", fields) as cursor:
-        for row in cursor:
-            print('{0}, {1}'.format(row[0], row[1]))
+
 # Spatial Join on closest road within 30'. Place in the created workspace
 def joinfeature():
     print "Beginning the joining of Roads data to Patches."
-    # Process: Spatial Join
-    patch = "patchlayer"
-    GIS_Data_A1_Roads = "GIS_Data.A1.Roads"
-    patch_SpatialJoin10 = "C:\\Users\\MiguelTo\\Documents\\ArcGIS\\Default.gdb\\patch_SpatialJoin10"
-    arcpy.SpatialJoin_analysis(patch, roadfeatures, patch_SpatialJoin10, "JOIN_ONE_TO_ONE", "KEEP_ALL",
-                               "FACILITYID \"FACILITYID\" true true false 4 Long 0 0 ,First,#,patch,FACILITYID,-1,-1;ROADID \"ROADID\" true true false 4 Long 0 0 ,First,#,patch,ROADID,-1,-1;FACILITYID_1 \"FACILITYID\" true true false 20 Text 0 0 ,First,#,GIS_Data.A1.Roads,FACILITYID,-1,-1;FACILITY_1 \"FACILITYID1\" true true false 8 Double 8 38 ,First,#,GIS_Data.A1.Roads,FACILITY_1,-1,-1",
-                               "CLOSEST", "100 Feet", "")
-
-# Attribute Transfer Source New Patches Roads.FACILITY_1 => Patches.ROADID
-    print "Data integration completed. \n"
+    arcpy.SpatialJoin_analysis("patchlayer", roadfeatures, outputFeatures, "JOIN_ONE_TO_ONE", "KEEP_ALL",
+                               "FACILITYID \"FACILITYID\" true true false 4 Long 0 0 ,First,#,patch,FACILITYID,-1,-1;SHAPE_Length \"SHAPE_Length\" false true true 8 Double 0 0 ,First,#,patch,SHAPE_Length,-1,-1;SHAPE_Area \"SHAPE_Area\" false true true 8 Double 0 0 ,First,#,patch,SHAPE_Area,-1,-1;FACILITYID_1 \"FACILITYID\" true true false 20 Text 0 0 ,First,#,roadsSubset,FACILITYID,-1,-1;LEGACYID \"LegacyID\" true true false 20 Text 0 0 ,First,#,roadsSubset,LEGACYID,-1,-1;FACILITY_1 \"FACILITYID1\" true true false 8 Double 0 0 ,First,#,roadsSubset,FACILITY_1,-1,-1",
+                               "CLOSEST", "30 Feet", "")
+    # Attribute Transfer Source New Patches Roads.FACILITY_1 => Patches.ROADID
+    print "Data join complete. \n"
 
 
 def attributetransfer():
-    fields = ['FACILITYID_1', 'FACILITY_1']
-    # cursor = arcpy.SearchCursor(outputFeatures, fields)
-    # index = {}
-    with arcpy.SearchCursor(outputFeatures, fields) as cursor:
-        for row in cursor:
-            print('{0}, {1}'.format(row[0], row[1]))
+    fields = ['FACILITYID', 'FACILITY_1']
+    cursor = arcpy.SearchCursor(outputFeatures, fields)
+    index = {}
+    for n in cursor:
+        index[n.getValue('FACILITYID')] = int(n.getValue('FACILITY_1'))
+    del cursor
 
-    # for n in cursor:
-    #     index[n.getValue('FACILITYID')] = int(n.getValue('FACILITY_1'))
-    #     print index
-    # del cursor
-    #
-    # cursor = arcpy.UpdateCursor(patchfeatures)
-    # for n in cursor:
-    #     print n.getValue('FACILITYID')
-    #     if n.getValue('FACILITYID') in index:
-    #         print index[n.getValue('FACILITYID')]
-    #         n.setValue('ROADID', index[n.getValue('FACILITYID')])
-    #         cursor.updateRow(n)
+    cursor = arcpy.UpdateCursor(patchfeatures)
+    for n in cursor:
+        if n.getValue('FACILITYID') in index:
+            n.setValue('ROADID', index[n.getValue('FACILITYID')])
+            cursor.updateRow(n)
 
 
 def UpdateFACILITYID():
@@ -82,9 +57,8 @@ def UpdateFACILITYID():
         return
 
     maxId = row.getValue("FACILITYID")
-    print row.getValue("FACILITYID")
-    # if maxId is None:
-    #     maxId = 0
+    if maxId is None:
+        maxId = 0
 
     # convert the id to an integer
     newId = int(maxId) + 1
@@ -104,8 +78,7 @@ def UpdateFACILITYID():
     return
 
 
-# UpdateFACILITYID()
+UpdateFACILITYID()
 selectdata()
 joinfeature()
-# testdata()
-# attributetransfer()
+attributetransfer()
