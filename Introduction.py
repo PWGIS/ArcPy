12dbde7eddaca2e720a427b74814bf90634e34ec
Sean Doig <sean.doig@durhamnc.gov>
2018-03-01 16:43:19 -0500

    Origin

--- Introduction.py
+++ Introduction.py
@@ -1,1 +1,5 @@
-
+# Write three print statements.
+# The first should print your first name
+# The second should print your Job Title
+# The third should print your favorite number
+# Sample: print "Hello World!"
