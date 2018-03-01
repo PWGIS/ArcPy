6dbaed043728cf8684293486c08371006bcc6546
Sean Doig <sean.doig@durhamnc.gov>
2018-03-01 16:35:12 -0500

    Revert "ORIGINAL"
    This reverts commit a79b5b64da03c9c8ead98d7c63149d596cb73796.

--- Introduction.py
+++
@@ -1,5 +0,0 @@
-# Write three print statements.
-# The first should print your first name
-# The second should print your Job Title
-# The third should print your favorite number
-# Sample: print "Hello World!"
