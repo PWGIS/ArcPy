320ccc3f214e84841b35d777eb74e8f0bc946a49
Sean Doig <sean.doig@durhamnc.gov>
2018-03-01 16:35:03 -0500

    seando: test 2

--- Introduction.py
+++ Introduction.py
@@ -3,11 +3,3 @@
 # The second should print your Job Title
 # The third should print your favorite number
 # Sample: print "Hello World!"
-
-print "Miguel Alan Torres"
-print "GIS Technician I"
-print "2048"
-
-print "Sean"
-print "GIS Coordinator"
-print "What?"
