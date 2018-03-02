5e5029969a6b044f59383bf9ea803d73bff6cc26
EHEno <erika.eno@durhamnc.gov>
2018-03-02 15:00:53 -0500

    Made something nice for you
    a home

--- Introduction.py
+++ Introduction.py
@@ -58,3 +58,62 @@ MTNum = "2048"
 EENum = "13"
 SDNum = "32"
 
+Number = int(JBNum) + int(MTNum) + int(EENum) + int(SDNum)
+print "Here's the number:"
+print Number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from turtle import *      # use the turtle library
+space = Screen()          # create a turtle screen (space)
+bob = Turtle()            # create a turtle named bob
+
+# Make a square
+bob.forward(100)          # tell bob to move forward by 100 units
+bob.right(90)             # turn right by 90 degrees
+bob.forward(100)          # tell bob to move forward by 100 units
+bob.right(90)             # turn right by 90 degrees
+bob.forward(100)          # tell bob to move forward by 100 units
+bob.right(90)             # turn right by 90 degrees
+bob.forward(100)          # tell bob to move forward by 100 units
+
+# Position for roof
+bob.right(90)
+
+# Make a roof
+bob.forward(100)          # tell bob to move forward by 100 units
+bob.right(-120)           # turn LEFT by 120 degrees
+bob.forward(100)          # tell bob to move forward by 100 units
+bob.right(-120)           # turn LEFT by 120 degrees
+bob.forward(100)          # tell bob to move forward by 100 units
+bob.right(-120)           # turn LEFT by 120 degrees
+
+
+print "And I made you a house you're welcome."
