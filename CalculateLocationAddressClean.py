fe4fd182a69149f0c91da9fd95b46f1871ea4a99
EHEno <erika.eno@durhamnc.gov>
2018-05-09 11:53:33 -0400

    Clean Up Codes
    Delete not needed lines, added comments.

--- CalculateLocationAddressClean.py
+++ CalculateLocationAddressClean.py
@@ -23,6 +23,7 @@ parentVersion = "Erikaki.UTILEDITS_EE"
 ##Datasets
 WaterSystem = "Database Connections/A1_durham-gis.sde/gis_data.A1.WaterSystem" #feature dataset
 SewerSystem = "Database Connections/A1_durham-gis.sde/gis_data.A1.SewerSystem" #feature dataset
+StormSystem = "Database Connections/A1_durham-gis.sde/gis_data.A1.StormWater" #feature dataset
 
 ##Feature Classes
 Hydrants = "Database Connections/PublicWorks_tax_sql_Erika.sde/Publicworks.PUBLICWORKS.WaterSystem/Publicworks.PUBLICWORKS.wnHydrant"
@@ -33,15 +34,15 @@ AddressPoints = "Database Connections/A1_durham-gis.sde/GIS_Data.A1.AddressFeatu
 Parcels = "Database Connections/A1_durham-gis.sde/GIS_Data.A1.TaxData/gis_data.A1.Parcels"
 
 #If you want to use a Geodatabase comment this in, once I switched to version I did not need this.
-# You can use this first inorder to create a copy of the Water System before editing. 
+# You can use this first in order to create a copy of the Utility Systems before editing.
 
 def MakeGDB():
-    LogMessage ("***************************START***************************************")
+    LogMessage ("***************************STARTING************************************")
     LogMessage(" Geodatabase creation...")
     FileGDBName="SystemBU" + today
     OutputLocation= "C:/Work"
     arcpy.CreateFileGDB_management(OutputLocation, FileGDBName)
-    # Set workspace
+    # Set workspace as the gdb you just created.
     arcpy.env.workspace="C:/Work/SystemBU" + today + ".gdb"
     LogMessage ("Switched Workspace")
     thisWorkspace = arcpy.env.workspace
@@ -58,7 +59,6 @@ def MakeGDB():
     LogMessage(FileGDBName + "/StormSystem_CopyFeatures")
     arcpy.Copy_management(StormSystem, thisWorkspace + "/StormSystem", "FeatureDataset")
     LogMessage(" Storm system copied.")
-
 
     return    
 
@@ -84,66 +84,50 @@ def CreateVersion():
     arcpy.ChangeVersion_management("soCasingLayer", "TRANSACTIONAL", "ERIKAKI." + versionName, "")
 
 
-def CalcAddressSub():
-    LogMessage("Starting Address Calc")
+def CreateJoins():
+    LogMessage("Starting Join Creations")
 
+## This is the locations of the output join tables that will be referenced later (** Should Update to be the new gdb created earlier. Because if a join already exisits inside with the same name it will error out**)
     out_feature_class = "C:/Work/Address.gdb/JoinWithin"
     out_feature_class1 = "C:/Work/Address.gdb/JoinClosest"
+    LogMessage("Made Join Outputs")
 
-    # arcpy.MakeFeatureLayer_management(feature,LayerName, "", "", "")
-    LogMessage("Made Outputs")
-    
-    arcpy.SpatialJoin_analysis("soCasingLayer", Parcels, out_feature_class, "JOIN_ONE_TO_ONE" , "KEEP_ALL","", "COMPLETELY_WITHIN","","")
-    LogMessage("Spatial Join Within Created")
-    arcpy.SpatialJoin_analysis(out_feature_class, Parcels, out_feature_class1, "JOIN_ONE_TO_ONE", "KEEP_ALL", "", "CLOSEST","","")
+## Create Spatial Join Layer to Parcels: Features that are completely within
 
+    # arcpy.SpatialJoin_analysis("soCasingLayer", Parcels, out_feature_class, "JOIN_ONE_TO_ONE" , "KEEP_ALL","", "COMPLETELY_WITHIN","","")
+    # LogMessage("Spatial Join Within Created")
+
+    ## Need to add code exit hwere if no features are within parcel to keep going to next line
+
+## Create Spatial Join Layer to Parcels: Features that are closest (THIS IS WERE WE NEED TO INTEGRATE THE ADDRESS POINT CODE)
+
+    arcpy.SpatialJoin_analysis("soCasingLayer", Parcels, out_feature_class1, "JOIN_ONE_TO_ONE", "KEEP_ALL", "", "CLOSEST","","")
     LogMessage("Spatial Join Closest Created")
 
     LogMessage("Start Join Data to Layer")
-##    AddJoin_management (in_layer_or_view, in_field, join_table, join_field, {join_type})
+## Joins the layer you want to update with the closest output join. ** We need to move this to the completly within part and then calc completely within then do the ones that are closest using miguels code.
+
+## AddJoin_management (in_layer_or_view, in_field, join_table, join_field, {join_type})
     arcpy.AddJoin_management("soCasingLayer", "OBJECTID", out_feature_class1, "TARGET_FID", "KEEP_ALL")
 
-##This isntworking
+## Selects all the records with NULL Location Fields
+
     arcpy.SelectLayerByAttribute_management("soCasingLayer", "NEW_SELECTION", "Publicworks.PUBLICWORKS.soCasing.LOCATIONDESCRIPTION IS NULL")
 
     result = arcpy.GetCount_management("soCasingLayer")
     LogMessage("There are %s records with NULL Location values..." % result)
 
     Cursor = arcpy.SearchCursor("soCasingLayer")
+    LogMessage("Field Calculate over Site_Address to Location")
 
     for row in Cursor:
-        LogMessage("Field Calculate over Site_Address to Location")
+
         ##CalculateField_management (in_table, field, expression, {expression_type}, {code_block})
-        arcpy.CalculateField_management("soCasingLayer", "Publicworks.PUBLICWORKS.soCasing.LOCATIONDESCRIPTION","[JoinClosest.SITE_ADDRE_1]", "VB", )
-        LogMessage("Nearest Addresses Calculated for Layer")
+        arcpy.CalculateField_management("soCasingLayer", "Publicworks.PUBLICWORKS.soCasing.LOCATIONDESCRIPTION","[JoinClosest.SITE_ADDRE]", "VB", )
 
-#     LogMessage("Field Calculate over Site_Address to Location")
-#
-#
-# # Add AddressFromAP() Call here
-#
-#
-# #CalculateField_management (in_table, field, expression, {expression_type}, {code_block})
-#     arcpy.CalculateField_management(LayerName, "Publicworks.PUBLICWORKS.wnHydrant.LOCATIONDESCRIPTION","[HydrantJoin.SITE_ADDRE], "VB",)
-#     arcpy.CalculateField_management("CValveLayer", "Publicworks.PUBLICWORKS.snControlValve.LOCATIONDESCRIPTION", "[CNValveClosest1.SITE_ADDRE_1]", "VB", )
-#     LogMessage("Nearest Addresses Calculated for Layer")
+    LogMessage("Field Site_Address Calculated over to Location")
 
-def CalcAddressField():
-    LogMessage("Start field calc")
-
-    ##Must use field Name for Query not alias this doesnt work right now
-    arcpy.SelectLayerByAttribute_management("soCasingLayer", "NEW_SELECTION", "LOCATIONDESCRIPTION IS NULL")
-
-    result = arcpy.GetCount_management("soCasingLayer")
-    LogMessage("There are %s records with NULL Location values..." % result)
-
-    Cursor = arcpy.SearchCursor("soCasingLayer")
-
-    for row in Cursor:
-        LogMessage("Field Calculate over Site_Address to Location")
-        ##CalculateField_management (in_table, field, expression, {expression_type}, {code_block})
-        arcpy.CalculateField_management("soCasingLayer", "Publicworks.PUBLICWORKS.soCasing.LOCATIONDESCRIPTION","[JoinClosest1.SITE_ADDRE_1]", "VB", )
-        LogMessage("Nearest Addresses Calculated for Layer")
+## Add AddressInParcel() Call here
 
 
 def AddressInParcel(feature_class, final_join):
@@ -229,17 +213,8 @@ def Cleanup(layer):
     arcpy.ReconcileVersions_management(inWorkspace, "ALL_VERSIONS", parentVersion, "ERIKAKI." + versionName,  "LOCK_ACQUIRED", "ABORT_CONFLICTS", "BY_OBJECT", "FAVOR_EDIT_VERSION", "POST", "DELETE_VERSION")
     LogMessage( "Finished")
 
-
-def GetCount():
-    arcpy.SelectLayerByAttribute_management("soCasingLayer", "NEW_SELECTION", "LOCATIONDESCRIPTION IS NULL")
-    result = arcpy.GetCount_management("soCasingLayer")
-    LogMessage("There are %s records with NULL Location values..." % result)
-
 # MakeGDB()
 CreateVersion()
-# CalcAddress(Hydrants, "HydrantLayer","Publicworks.PUBLICWORKS.wnHydrant.LOCATIONDESCRIPTION", "[HydrantJoin.SITE_ADDRE]")
-CalcAddressSub()
-# CalcAddressField()
-# GetCount()
+CreateJoins()
 #Cleanup("soCasingLayer")
 
