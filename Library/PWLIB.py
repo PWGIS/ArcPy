f9b0709decce866452fb2668b3b3502d82948514
Miguel Torres <miguel.torres@durhamnc.gov>
2018-05-16 14:20:13 -0400

    added udpatefacilityid() function

--- Library/PWLIB.py
+++ Library/PWLIB.py
@@ -112,5 +112,42 @@ def transcribe(message, file_path = os.path.dirname(__file__)+ "/"):
     txt_file.close()
 
 
+def updatefacilityid(featurelayer):
+    """ ARGS:
+    featurelayer: A reference to a layer that contains the featureclass that has to be updated.
+
+    DESCRIPTION:
+    Function takes in a feature layer and isolates the highest FACILITYID value.
+    Then it searches for records with a FACILITYID of NULL and then updates them
+        in sequential order.
+        """
+    # Find the max value here, if we get nothing then quit.
+    print "Create Search Cursor"
+    rows = arcpy.SearchCursor(featurelayer, "", "", "", "FACILITYID D")
+    row = rows.next()
+    if row is None:
+        del rows
+        del row
+        return
+
+    maxId = row.getValue("FACILITYID")
+    if maxId is None:
+        maxId = 0
+    # convert the id to an integer
+    newId = int(maxId) + 1
+
+    # Create update cursor for feature class, only for those features whose FACILITYID is NULL.
+    rows = arcpy.UpdateCursor(patchfeatures, "FACILITYID IS NULL")
+
+    # Now update the FACILITYIDs for those features...
+    for row in rows:
+        row.setValue('FACILITYID', newId)
+        rows.updateRow(row)
+        newId = newId + 1
+
+    # Delete cursor and row objects to remove locks on the data
+    del rows
+
+
 if __name__ == "__main__":
     logmessage("is this working?")
