63ce9bac59f13fbd1da6cba05d2cc8e8ee7e4861
Miguel Torres <miguel.torres@durhamnc.gov>
2018-07-19 09:21:13 -0400

    adjustments
    removed AddressInParcel()

--- Library/PWLIB.py
+++ Library/PWLIB.py
@@ -1,81 +1,6 @@
 import arcpy, time, os
 
 
-def addressinparcel(feature_class, final_join):
-    """ ARGS
-        feature_class : The feature class that is being updated.
-        final_join :    The dialog path to the join that has the feature class being updated.
-                        Final Join is expected to be joined as follows;
-                        Final Join must be a point feature class.
-                        Feature class joined to parcels containing the feature.
-                        The result being joined to parcels nearest the feature.
-
-        ASSUMPTIONS     - The name of the field being updated is "LOCATION"
-
-        Takes a Feature Class and updates the location field and updates
-        it with the site address of the Address Point contained within the same parcel.
-        If there is more than one Address Point, it will take the nearest address point to the feature.
-        If there are no Address Points, it will take the site address sof the parcel."""
-
-    # determine if feature_class is a line. if so, convert to centroid.
-    if arcpy.Describe(feature_class).shapeType != "Point":
-        logmessage( "Error Second Argument must be a Point Feature Class")
-        return
-
-    # create Layers; Features, Parcels, Address Points
-    arcpy.MakeFeatureLayer_management(feature_class, "Feature_Layer")
-    logmessage( "Feature Layer Created.")
-    arcpy.MakeFeatureLayer_management("Database Connections/A1_durham-gis.sde/GIS_Data.A1.TaxData/GIS_Data.A1.Parcels",
-                                      "Parcels_Layer")
-    logmessage( "Parcels Layer Created.")
-    arcpy.MakeFeatureLayer_management(
-        "Database Connections/A1_durham-gis.sde/GIS_Data.A1.AddressFeatures/GIS_Data.A1.ActiveAddressPoints",
-        "AP_Layer")
-    logmessage( "Address Points Layer Created.")
-    # iterate through features that are within a parcel. "PARCEL_ID IS NOT NULL"
-    with arcpy.da.SearchCursor(final_join, ["Parcel_ID", "PARCEL_ID_1", "FACILITYID"],
-                               where_clause='PARCEL_ID IS NOT NULL') as cursor:
-        for row in cursor:
-            # select that parcel
-            arcpy.SelectLayerByAttribute_management("Parcels_Layer", "NEW_SELECTION", "[PARCEl_ID] = " + str(row[0]))
-            # Select APs within the selected parcel
-            arcpy.SelectLayerByLocation_management("AP_Layer", "WITHIN", "Parcels_Layer")
-            transcribe(str(row[0]) + " Parcel contains " + str(arcpy.GetCount_management("AP_Layer")) + " Address Point(s)")
-            # If AP Count is 1, move the AP Address to the feature.
-            if int(arcpy.GetCount_management("AP_Layer")[0]) == 1:
-                arcpy.SelectLayerByAttribute_management("Feature_Layer", "NEW_SELECTION",
-                                                        "[FACILITYID] = '" + str(row[2]) + "'")
-                logmessage("\tTransferring Address to swNode " + str(row[2]))
-                transcribe("\tTransferring Address to swNode " + str(row[2]))
-                ap_cursor = arcpy.da.SearchCursor("AP_Layer", ["SITE_ADDRE"])
-                for AP in ap_cursor:
-                    transcribe("\t" + AP[0])
-                    arcpy.CalculateField_management("Feature_Layer", "LOCATION", "\"" + AP[0] + "\"", "", "")
-                del ap_cursor
-            # If AP Count is > 1,
-            # Create a spatial join using selected feature and selected address points, then choose the nearest one
-            elif int(arcpy.GetCount_management("AP_Layer")[0]) > 1:
-                arcpy.SpatialJoin_analysis("Feature_Layer", "AP_Layer", "in_memory/NearestAP", "JOIN_ONE_TO_ONE",
-                                           "KEEP_ALL", "", "CLOSEST")
-                arcpy.MakeFeatureLayer_management("in_memory/NearestAP", "NearestAP_Layer")
-                transcribe("\tTransferring Address to swNode " + str(row[2]))
-                ap_cursor = arcpy.da.SearchCursor("NearestAP_Layer", ["SITE_ADDRE"])
-                for AP in ap_cursor:
-                    transcribe("\t" + AP[0])
-                    arcpy.CalculateField_management("Feature_Layer", "LOCATION", "\"" + AP[0]+ "\"", "", "")
-                del ap_cursor
-            # If AP Count == 0, move the Parcel's Site Address to the feature
-            elif int(arcpy.GetCount_management("AP_Layer")[0]) == 0:
-                arcpy.SelectLayerByAttribute_management("Feature_Layer", "NEW_SELECTION",
-                                                        "[FACILITYID] = '" + str(row[2]) + "'")
-                transcribe("\tTransferring Address to swNode " + str(row[2]))
-                parcel_cursor = arcpy.da.SearchCursor("Parcel_Layer", ["SITE_ADDRE"])
-                for parcel in parcel_cursor:
-                    transcribe("\t" + parcel[0])
-                    arcpy.CalculateField_management("Feature_Layer", "LOCATION", "\"" + parcel[0] + "\"", "", "")
-                del parcel_cursor
-
-
 def createLayers(layers, workspace):
     """
     Takes the name or list of names of feature classes and creates layers if they exist in the feature datasets.
@@ -129,7 +54,7 @@ def logmessage(message):
     PARAMETERS:
     message (str): a string variable to be written to the file and console
     """
-    print time.strftime ("%H:%M", time.localtime()) + "\t" + str(message)
+    print time.strftime("%H:%M", time.localtime()) + "\t" + str(message)
     return
 
 
--- Library/README.txt
+++ Library/README.txt
@@ -6,14 +6,6 @@ GIHUB Repo: https://github.com/PWGIS/ArcPy/tree/master/Library
 ------------------------------------------------------
 		FUNCTIONS
 ------------------------------------------------------
-AddressInParcel
-(feature_class, final_join)
-Bboth arguments are layers that contain the respective items.
-Takes a Feature Class and updates the location field and updates
-it with the site address of the Address Point contained within the same parcel.
-If there is more than one Address Point, it will take the nearest address point to the feature.
-If there are no Address Points, it will take the site address sof the parcel.
-
 CreateLayers
 (list/string, string)
 Takes the name or list of names of feature classes and creates layers if they exist in the feature datasets.
@@ -50,6 +42,7 @@ If the version already exists, it will not create a version but will still chang
 ------------------------------------------------------
 		UPDATES
 ------------------------------------------------------
+7/19/2018:	Removed AddressInparcel() function.
 7/5/2018: 	Added the createLayers, mergeVersion, and versionedLayers functions.
 6/26/2018: 	BUG FIX - Transcribe now converts data argument into String type. 
 6/13/2018: 	BUG FIX - Transcribe now parses for the file name differently.
