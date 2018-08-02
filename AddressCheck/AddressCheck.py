5370671ab3b7ef49208718d68f15d3b0034cfb01
Miguel Torres <miguel.torres@durhamnc.gov>
2018-08-02 15:19:24 -0400

    Update for sean in progress

--- AddressCheck/AddressCheck.py
+++ AddressCheck/AddressCheck.py
@@ -8,8 +8,7 @@ connection = arcpy.env.workspace = "D:/BackupRepo.gdb"
 
 def main():
     #why do you create the addressLyr here but the parcelLyr inside the addressCheck function?
-    arcpy.MakeFeatureLayer_management(connection + "/ActiveAddressPoints",
-                                      "addressLyr")
+
     addressCheck()
 
 
@@ -17,34 +16,45 @@ def addressCheck():
     print "Beginning Address Check"
     parcelCursor = arcpy.da.SearchCursor(connection + "/Parcels",
                                          ['PARCEL_ID', 'SITE_ADDRE', 'SHAPE@'])
-    arcpy.MakeFeatureLayer_management(connection + "/Parcels", "parcelLyr", "PIN NOT LIKE '%.%' OR PIN LIKE '%.000' OR PIN LIKE '%.L00' OR PIN LIKE '%.DO' OR PIN LIKE '%.DW' OR PIN LIKE '%.DG' OR PIN LIKE '%.SPL'")
+    arcpy.MakeFeatureLayer_management(connection + "/ActiveAddressPoints",
+                                      "addressLyr")
+    arcpy.MakeFeatureLayer_management(connection + "/Parcels", "parcelLyr",
+                                      "PIN NOT LIKE '%.%' OR PIN LIKE '%.000' OR PIN LIKE '%.L00' OR PIN LIKE '%.DO' "
+                                      "OR PIN LIKE '%.DW' OR PIN LIKE '%.DG' OR PIN LIKE '%.SPL'")
     totalParcels = int(arcpy.GetCount_management("parcelLyr")[0])
-    #Miguel, keep in mind you that this code should not only be clear to you, but to others who read it.  So tell us what you are using i and n for
-    i = 0
-    n = 0
+    i = 0  # i is used in the progress bar to keep track of the number of '.' following "WORKING"
+    n = 0  # n is number of parcels checked, this is for the percentage in progress bar
     for parcel in parcelCursor:
         # logmessage(parcel)
         n+=1
         if i > 3:
             i = 0
-        #Miguel, you are using modules that others (including me) aren't familiar with.  yes, people could look them up, but
-        # they will learn more if you just tell them.  Always remember you are not working with programmers, or people who
-        # aspire to be programmers, so don't hold them to that standard.  If you are going to be "clever" you need to leave
-        # breadcrumbs so others can follow you.
 
+        # sys.stdout is the low-level print function that allows you to have more control of the output
+        # as a result, it is able to write a line without moving to the next line.
+        # In all cases this is used for the progress bar or to operate around it.
         sys.stdout.write("\r[" + str(round(100 * float(n)/float(totalParcels), 2)) + "%]\tWorking" + "%s" % '.' * i)
         sys.stdout.flush()
         arcpy.SelectLayerByLocation_management("addressLyr", "INTERSECT", parcel[2], "", "NEW_SELECTION")
         if int(arcpy.GetCount_management("addressLyr")[0]) == 1:
-            apCursor = arcpy.da.SearchCursor("addressLyr", ['ID', 'SITE_ADDRE', 'PARCEL_ID'])  #I added Parcel ID in here because I realized I was checking that too.
+            apCursor = arcpy.da.SearchCursor("addressLyr", ['ID', 'SITE_ADDRE', 'PARCEL_ID'])
             next(apCursor)
-            if parcel[1].strip() != apCursor[1].strip():  #explain strip, especially since people may just be familiar with trim.  at some point you won't need to but do it now.
+            # .strip is a string method that removes the "empty space" flanking both sides of a string.
+            # e.g., "       This      " becomes "This"
+            if parcel[1].strip() != apCursor[1].strip():
                 sys.stdout.write("\r")
                 sys.stdout.flush()
-                #along the same vein.  for right now i want you to give an example of what the code below will produce
+                # when the Parcel address doesn't match the AP address or the Parcel IDs do not match
+                # it will print the Parcel and AddressPoint ID their respective addresses and the AP parcel ID
                 transcribe("1:1 Address match error: \n\t\tParcelID: " + parcel[0] + ", " + parcel[1] +
                            "\n\t\tAddressPointID: " + str(int(apCursor[0])) + ", " + str(apCursor[1]))
-            # let's add a few lines here to compare the parcelIDs.  I suggest you call the transcribe function after you do that.
+            if parcel[0] != apCursor[2]:
+                sys.stdout.write("\r")
+                sys.stdout.flush()
+                # when the Parcel address doesn't match the AP address or the Parcel IDs do not match
+                # it will print the Parcel and AddressPoint ID their respective addresses and the AP parcel ID
+                transcribe("1:1 Parcel_ID match error: \n\t\tParcelID: " + parcel[0] +
+                           "\n\t\tAddressPointPID: " + str(int(apCursor[0])) + ", " + str(int(apCursor[2])))
 
         #As we just discussed i don't think we should compare the site addresses when there is more than one address point on a parcel since
         # most of those address points won't match and, according to Carmen, there's no way to identify the master address point by the attributes
